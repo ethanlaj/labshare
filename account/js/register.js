0a3223aff30a73749717b62b4998db6f91e2d802
Ethan Lajeunesse <ethan@lajfam.com>
2022-09-24 21:18:42 -0400

    Add subfolders

--- account/js/register.js
+++ account/js/register.js
@@ -1,16 +1,10 @@
 window.addEventListener("load", init);
 
 function init() {
-    let login = document.getElementById("login");
-    button.addEventListener("click", loginLink);
-
     let register = document.getElementById("register");
     register.addEventListener("click", registerLink);
 }
 
-function loginLink() {
-    window.location.href = ".././login.html";
-}
 function registerLink() {
     window.location.href = ".././register.html";
 }
