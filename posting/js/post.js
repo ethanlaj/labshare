492cb4340bd48bb5d88393a86724ccb4e2de4a81
Ethan Lajeunesse <ethan@lajfam.com>
2022-09-28 12:07:32 -0400

    Remove onclicks from reply.js

--- posting/js/post.js
+++ posting/js/post.js
@@ -1,93 +1,172 @@
-function lastClicked(comment) {
-	lastClickedComment = comment;
-}
+(function () {
+	"use strict";
 
-function startReply(comment) {
-	let replyStatus = comment.getAttribute('replying');
+	window.addEventListener('load', init);
+	let lastClickedComment;
 
-	if (replyStatus == "false") {
-		comment.setAttribute('replying', 'true');
+	/**
+	 * Initial function that is ran when the window loads
+	 * Adds event listeners to:
+	 * - Post buttons
+	 * - Reply buttons
+	 * - Comment buttons
+	 */
+	function init() {
+		// Post buttons
+		let savePostBtn = document.getElementById("savePostBtn");
+		savePostBtn.addEventListener("click", savePost);
 
-		comment.querySelector(".replyButton").classList.replace("btn-secondary", "btn-primary")
-		comment.querySelector(".cancelReply").hidden = false;
-		comment.querySelector(".replyBox").hidden = false;
-	} else {
-		let reply = comment.querySelector('.replyBox');
-		if (reply.value.trim() == "")
-			return cancelReply(comment);
+		let applyToPostBtn = document.getElementById("applyToPostBtn");
+		applyToPostBtn.addEventListener("click", applyToPost);
 
-		console.log(`Replied to ${comment.id} with "${reply.value}"`);
-		reply.value = "";
-		cancelReply(comment);
+		let reportPostBtn = document.getElementById("reportPostBtn");
+		reportPostBtn.addEventListener("click", reportPost);
+
+		let deletePostBtn = document.getElementById("deletePostBtn");
+		deletePostBtn.addEventListener("click", deletePost);
+
+		// Reply buttons
+		let replyButtons = document.getElementsByClassName("replyButton");
+		for (let replyButton of replyButtons)
+			replyButton.addEventListener("click", startReply);
+
+		let cancelReplyButtons = document.getElementsByClassName("cancelReply");
+		for (let cancelReplyButton of cancelReplyButtons)
+			cancelReplyButton.addEventListener("click", cancelReply);
+
+		// Comment buttons
+		let initEditCommentBtns = document.querySelectorAll(".dropdown-item.edit");
+		for (let initEditCommentBtn of initEditCommentBtns)
+			initEditCommentBtn.addEventListener("click", initEditComment);
+
+		let initDeleteCommentBtns = document.querySelectorAll(".dropdown-item.delete");
+		for (let initDeleteCommentBtn of initDeleteCommentBtns)
+			initDeleteCommentBtn.addEventListener("click", lastClicked);
+
+		let initReportCommentBtns = document.querySelectorAll(".dropdown-item.report");
+		for (let initReportCommentBtn of initReportCommentBtns)
+			initReportCommentBtn.addEventListener("click", lastClicked);
+
+		let reportCommentBtn = document.getElementById("reportCommentBtn");
+		reportCommentBtn.addEventListener("click", reportComment);
+
+		let addCommentBtn = document.getElementById("addCommentBtn");
+		addCommentBtn.addEventListener("click", addComment);
+
+		let editCommentBtn = document.getElementById("editCommentBtn");
+		editCommentBtn.addEventListener("click", editComment);
+
+		let deleteCommentBtn = document.getElementById("deleteCommentBtn");
+		deleteCommentBtn.addEventListener("click", deleteComment);
 	}
-}
 
-function cancelReply(comment) {
-	comment.setAttribute('replying', 'false');
+	function lastClicked() {
+		let comment = $(this.closest('.comment'))[0];
 
-	comment.querySelector(".replyButton").classList.replace("btn-primary", "btn-secondary")
-	comment.querySelector(".cancelReply").hidden = true;
-	comment.querySelector(".replyBox").hidden = true;
-}
+		lastClickedComment = comment;
+	}
 
-function savePost() {
-	console.log("Saved post");
-	$("#save").modal("show");
-}
+	function startReply() {
+		let comment = $(this.closest('.comment'))[0];
 
-function applyToPost() {
-	console.log("Applied to post");
-	$("#apply").modal("show");
-}
+		let replyStatus = comment.getAttribute('replying');
 
-function reportPost() {
-	console.log("Reported post");
-	$("#reportPost").modal("hide");
-	$("#reportReceived").modal("show");
-}
+		if (replyStatus == "false") {
+			comment.setAttribute('replying', 'true');
 
-function deletePost() {
-	console.log("Deleted post");
-	$("#deletePost").modal("hide");
-	$("#postDeleted").modal("show");
-}
+			comment.querySelector(".replyButton").classList.replace("btn-secondary", "btn-primary")
+			comment.querySelector(".cancelReply").hidden = false;
+			comment.querySelector(".replyBox").hidden = false;
+		} else {
+			let reply = comment.querySelector('.replyBox');
+			if (reply.value.trim() == "")
+				return noReplyView(comment);
 
-function reportComment(comment = lastClickedComment) {
-	console.log(`Reported comment ${comment.id}`);
-	$("#reportComment").modal("hide");
-	$("#reportReceived").modal("show");
-}
+			console.log(`Replied to ${comment.id} with "${reply.value}"`);
+			reply.value = "";
+			noReplyView(comment);
+		}
+	}
 
-function addComment() {
-	let modalTextArea = document.getElementById("commentAddTextForm");
-	console.log(`Added comment ${modalTextArea.value}`)
-	modalTextArea.value = "";
+	function cancelReply() {
+		let comment = $(this.closest('.comment'))[0];
 
-	$("#addComment").modal("hide");
-}
+		noReplyView(comment);
+	}
 
-function initEditComment(comment) {
-	lastClickedComment = comment;
+	function noReplyView(comment) {
+		comment.setAttribute('replying', 'false');
 
-	let modalTextArea = document.getElementById("commentEditTextForm");
-	let innerText = comment.querySelector('.commentContent').innerText;
-	modalTextArea.value = innerText
-	modalTextArea.rows = innerText.length / 35;
+		comment.querySelector(".replyButton").classList.replace("btn-primary", "btn-secondary")
+		comment.querySelector(".cancelReply").hidden = true;
+		comment.querySelector(".replyBox").hidden = true;
+	}
 
-	$("#editComment").modal("show");
-}
+	function savePost() {
+		console.log("Saved post");
+		$("#save").modal("show");
+	}
 
-function saveEditComment() {
-	let commentToEdit = lastClickedComment;
-	let modalTextArea = document.getElementById("commentEditTextForm")
+	function applyToPost() {
+		console.log("Applied to post");
+		$("#apply").modal("show");
+	}
 
-	commentToEdit.querySelector('.commentContent').innerText = modalTextArea.value;
-	console.log(`Edited ${commentToEdit.id} to ${modalTextArea.value}`)
-	$("#editComment").modal("hide");
-}
+	function reportPost() {
+		console.log("Reported post");
+		$("#reportPost").modal("hide");
+		$("#reportReceived").modal("show");
+	}
 
-function deleteComment(comment = lastClickedComment) {
-	console.log(`Deleted comment ${comment.id}`);
-	$("#deleteComment").modal("hide");
-	$("#commentDeleted").modal("show");
-}
+	function deletePost() {
+		console.log("Deleted post");
+		$("#deletePost").modal("hide");
+		$("#postDeleted").modal("show");
+	}
+
+	function reportComment() {
+		let comment = lastClickedComment;
+
+		console.log(`Reported comment ${comment.id}`);
+		$("#reportComment").modal("hide");
+		$("#reportReceived").modal("show");
+	}
+
+	function addComment() {
+		let modalTextArea = document.getElementById("commentAddTextForm");
+		console.log(`Added comment ${modalTextArea.value}`)
+		modalTextArea.value = "";
+
+		$("#addComment").modal("hide");
+	}
+
+	function initEditComment() {
+		let comment = $(this.closest('.comment'))[0];
+
+		lastClickedComment = comment;
+
+		let modalTextArea = document.getElementById("commentEditTextForm");
+		let innerText = comment.querySelector('.commentContent').innerText;
+		modalTextArea.value = innerText
+		modalTextArea.rows = innerText.length / 35;
+
+		$("#editComment").modal("show");
+	}
+
+	function editComment() {
+		let commentToEdit = lastClickedComment;
+		let modalTextArea = document.getElementById("commentEditTextForm")
+
+		commentToEdit.querySelector('.commentContent').innerText = modalTextArea.value;
+		console.log(`Edited ${commentToEdit.id} to ${modalTextArea.value}`)
+		$("#editComment").modal("hide");
+	}
+
+	function deleteComment() {
+		let comment = lastClickedComment;
+
+		console.log(`Deleted comment ${comment.id}`);
+		$("#deleteComment").modal("hide");
+		$("#commentDeleted").modal("show");
+	}
+})();
