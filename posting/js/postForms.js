6f3b36481d7738af9a5fa1eeba33e7d66d7c3a81
Ethan Lajeunesse <ethan@lajfam.com>
2022-09-28 11:17:18 -0400

    Add JSDocs to postForms.js, change to proper JS

--- posting/js/postForms.js
+++ posting/js/postForms.js
@@ -1,21 +1,52 @@
-window.onload = () => {
-	$("#postForm").submit(function (event) {
+(function () {
+	"use strict";
+
+	window.addEventListener('load', init);
+
+	/**
+	 * Initial function that is ran when the window loads
+	 * Adds event listener to form
+	 */
+	function init() {
+		let postForm = document.getElementById("postForm");
+		postForm.addEventListener("submit", checkInputs);
+	}
+
+	/**
+	 * Checks the form's input fields and does not allow
+	 * submission of form if it does not meet requirements
+	 * @param {SubmitEvent} event
+	 */
+	function checkInputs(event) {
 		let title = document.getElementById('title');
 		let content = document.getElementById('content');
-
 		if (validateTitle(title) == false || validateContent(content) == false)
 			event.preventDefault();
-	});
-}
+	}
 
-function validateTitle(titleElement) {
-	let str = titleElement.value;
+	/**
+	 * Checks to see if the title meets the length requirement
+	 * @param {HTMLElement} titleElement 
+	 * @returns {boolean}
+	 * true = meets length requirement;
+	 * false = does not meet length requirement
+	 */
+	function validateTitle(titleElement) {
+		let str = titleElement.value;
 
-	return (str.length >= 10 && str.length <= 50);
-}
+		return (str.length >= 10 && str.length <= 50);
+	};
 
-function validateContent(contentElement) {
-	let str = contentElement.value;
+	/**
+	 * Checks to see if the content meets the length requirement
+	 * @param {HTMLElement} contentElement 
+	 * @returns {boolean}
+	 * true = meets length requirement;
+	 * false = does not meet length requirement
+	 */
+	function validateContent(contentElement) {
+		let str = contentElement.value;
 
-	return (str.length >= 50 && str.length <= 2000);
-}
+		return (str.length >= 50 && str.length <= 2000);
+	};
+})();
