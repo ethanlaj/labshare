7e09e3145924f5fdad9455fd5dd42098d98748af
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2022-10-27 15:24:41 -0400

    Create test.php

---
+++ home/test.php
@@ -0,0 +1,24 @@
+<?PHP
+ // connect to baseball db
+
+echo 'My host name is ' .$_ENV["SP_HOST_NAME"];
+ 
+ // make sql
+//  $sql = "SELECT DISTINCT teamID,name FROM teams ORDER BY name;";
+//  $statement = $dbconnection->prepare($sql);
+ 
+//  // get list
+//  $statement->execute();
+//  $teams = $statement->fetchAll();
+//  // loop over list and display the names
+//  foreach($teams as $team) {
+//      echo "<p>";
+//      echo "<a href='years.php?teamID={$team['teamID']}'>";
+//      echo $team["name"] . " " . $team["teamID"];
+//      echo "</a>";
+//      echo "</p>";
+//  }
+
+
+
+?>
