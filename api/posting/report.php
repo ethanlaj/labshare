4aff6e8a48ebd2a5cc7a9496b12daaef7fbb8feb
Ethan Lajeunesse <ethan@lajfam.com>
2022-11-29 20:41:17 -0500

    Add documentation for posting API

--- api/posting/report.php
+++ api/posting/report.php
@@ -5,13 +5,13 @@ $VALID_TYPES = array(1, 2);
 if (isset($_POST["id"]) && isset($_POST["type"])) {
 	require_once(__DIR__ . "/../../database/postFunctions.php");
 
-	$post_id = $_POST["id"];
+	$id = $_POST["id"];
 	$type = (int) $_POST["type"];
 
 	if (!in_array($type, $VALID_TYPES))
 		return header("HTTP/1.1 400 Invalid type parameter");
 
-	report($post_id, $type);
+	report($id, $type);
 } else {
 	header("HTTP/1.1 400 Invalid parameters");
 }
