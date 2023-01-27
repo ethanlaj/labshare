06201247665018b93e5d6cd8cfbc7275847518f3
Ethan Lajeunesse <ethan@lajfam.com>
2023-01-27 16:48:31 -0500

    Init react app creation

--- api/notifications/getNotifications.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-
-header("Content-Type: application/json");
-
-require_once(__DIR__ . "/../../database/notificationFunctions.php");
-
-$notifications = get_notifications();
-
-echo json_encode($notifications);
