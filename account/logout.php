5d3b800b9b307915453d3b380647e246753f5b2c
Ethan Lajeunesse <ethan@lajfam.com>
2023-11-21 09:44:59 -0500

    Fix logout?

--- account/logout.php
+++ account/logout.php
@@ -43,7 +43,7 @@ if (isset($_SESSION["user"])) {
 	?>
 
 	<h1>You have successfully logged out</h1>
-	<h4>Thank you for using Science Proposal</h4>
+	<h4>Thank you for using LabShare</h4>
 	<a href="./login.php" class="btn btn-primary">Log Back In</a>
 </body>
 
